67d49d108c2c77bdc102832073f5b5ff343516eb
Niklas Wieland <niklaswielandhotmail.de>
2023-06-12 11:53:27 +0200

    fetch logic

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,21 @@ import { SWRConfig } from "swr";
 const fetcher = (url)=>fetch(url).then((response)=>response.json())
 
 export default function App({ Component, pageProps }) {
+  const {data,error,isLoading} = useSWR("https://example-apis.vercel.app/api/art")
+if(error)
+return 
+<div>failed to load</div>;
+if(isLoading)
+return 
+<div>loading...</div>;
+console.log(data)
   return (
     <>
     <SWRConfig value={{fetcher}}>
       <GlobalStyle />
       <Component {...pageProps} />
     </SWRConfig>
+    
     </>
   );
 }
